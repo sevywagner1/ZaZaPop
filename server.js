38dd68dc3af85eeba0d16746cc1bdb92e72bcd5c
Severin wagner <severin.wagner.365@k12.friscoisd.org>
2020-11-30 00:41:03 -0600

    updated for heroku

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto' == 'http']) {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
